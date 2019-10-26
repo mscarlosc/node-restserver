b318ccfda6af721c6bd7c2d2edf28c6629a04b4b
Carlos SC <mscarlosc@gmail.com>
2019-10-26 18:49:45 -0500

    Varible de entorno MONGO

--- server/config/config.js
+++ server/config/config.js
@@ -6,11 +6,9 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 
 // Entorno de Base de Datos
 let urlDB;
-if ( process.env.NODE_ENV = process.env.NODE_ENV === 'dev' ) {
+if ( process.env.NODE_ENV  === 'dev' ) {
     urlDB = 'mongodb://localhost:27017/bd_cafe';
 } else {
-    urlDB = 'mongodb+srv://admin:M0ngoAdmin27421042@cluster0-wjqqe.mongodb.net/bd_cafe';
+    urlDB = process.env.MONGO_URL;
 }
 process.env.URL_DB = urlDB;
-
-
